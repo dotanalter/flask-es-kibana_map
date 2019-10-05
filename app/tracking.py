547a7deb6ac534d6ef792034e6b55b092b23f6ee
Dotan <dotanalter@gmail.com>
2019-10-05 18:10:51 +0300

    added location to kibana

--- app/tracking.py
+++ app/tracking.py
@@ -4,6 +4,8 @@
 ##############################################
 from flask import Flask, jsonify, request
 from elasticsearch import Elasticsearch
+import requests
+import geocoder
 
 es = Elasticsearch(['elasticsearch'])
 app = Flask(__name__)
@@ -18,6 +20,7 @@ def sendJSONtoES():
 	usage = request.args['usage']
 	price = request.args['price'] 
 	currency = request.args['currency']
+	ip = request.remote_addr
 
 	########################################################
 	# checks if the price entered by the user is an integer:
@@ -26,9 +29,28 @@ def sendJSONtoES():
 		price = int(price)
 	except:
 		return "price parameter should be an integer"
-
-	json_string={"type":type,"product":product,"usage":usage,"price":price,"currency":currency,'ip': request.remote_addr} #JSON object from parameters + user's ip address
-	es.index(index='tracking', doc_type='tracking', body=json_string) #send the JSON object to elasticsearch index named "tracking"
+	requests.put('http://localhost:9200/tracking')
+	headers = {'Content-Type': 'application/json'}
+	data = '\n{\n  "properties": {\n        "location": {\n          "type": "geo_point"\n        }\n      }\n   }'
+	requests.put('http://localhost:9200/tracking/_mapping',headers=headers, data=data)
+	g = geocoder.ip(ip)
+	location = g.latlng
+	if not location:
+		g = geocoder.ip('me')
+		location = g.latlng
+	json_string= {
+		"type":type,
+		"product":product,
+		"usage":usage,
+		"price":price,
+		"currency":currency,
+		'ip': ip,
+		"location": { 
+    		"lat": location[0],
+    		"lon": location[1]
+  		}
+	} #JSON object from parameters + user's ip address
+	es.index(index='tracking', body=json_string) #send the JSON object to elasticsearch index named "tracking"
 	return ("JSON object sent to elasticsearch check out http://127.0.0.1:9200/tracking")		
 
 
